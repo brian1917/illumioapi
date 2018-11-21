f3f699605238be4ecfa543b41e673f5a4e9bd946
Brian Pitta <Brian.Pitta@illumio.com>
2018-11-20 20:24:09 -0500

    login returns type APIResponse

--- userslogin.go
+++ userslogin.go
@@ -3,6 +3,7 @@ package illumioapi
 import (
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -68,13 +69,14 @@ func Authenticate(pce PCE, username, password string) (Authentication, APIRespon
 }
 
 // Login takes an auth token and returns a session token
-func Login(pce PCE, authToken string) (UserLogin, error) {
+func Login(pce PCE, authToken string) (UserLogin, APIResponse, error) {
 	var login UserLogin
+	var response APIResponse
 
 	// Build the API URL
 	apiURL, err := url.Parse("https://" + pceSanitization(pce.FQDN) + ":" + strconv.Itoa(pce.Port) + "/api/v1/users/login")
 	if err != nil {
-		return login, fmt.Errorf("login error - %s", err)
+		return login, response, fmt.Errorf("login error - %s", err)
 	}
 
 	// Create HTTP client and request
@@ -85,7 +87,7 @@ func Login(pce PCE, authToken string) (UserLogin, error) {
 
 	req, err := http.NewRequest("GET", apiURL.String(), nil)
 	if err != nil {
-		return login, fmt.Errorf("login error - %s", err)
+		return login, response, fmt.Errorf("login error - %s", err)
 	}
 
 	// Set basic authentication and headers
@@ -95,18 +97,29 @@ func Login(pce PCE, authToken string) (UserLogin, error) {
 	// Make HTTP Request
 	resp, err := client.Do(req)
 	if err != nil {
-		return login, fmt.Errorf("login error - %s", err)
+		return login, response, fmt.Errorf("login error - %s", err)
 	}
 
 	// Process response
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return login, fmt.Errorf("login error - %s", err)
+		return login, response, fmt.Errorf("login error - %s", err)
+	}
+
+	// Put relevant response info into struct
+	response.RespBody = string(data[:])
+	response.StatusCode = resp.StatusCode
+	response.Header = resp.Header
+	response.Request = resp.Request
+
+	// Check for a 200 response code
+	if strconv.Itoa(resp.StatusCode)[0:1] != "2" {
+		return login, response, errors.New("login error - http status code of " + strconv.Itoa(response.StatusCode))
 	}
 
 	// Put relevant response info into struct
 	json.Unmarshal(data, &login)
 
-	return login, nil
+	return login, response, nil
 
 }
