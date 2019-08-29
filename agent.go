e17e67f72131bb6691065fcb31155fd1a69e28c6
Brian Pitta <Brian.Pitta@illumio.com>
2019-08-28 20:36:07 -0400

    remove commented code

--- agent.go
+++ agent.go
@@ -8,29 +8,6 @@ import (
 	"time"
 )
 
-//Test
-
-// Agent represents an Illumio VEN
-// type Agent struct {
-// 	ActivePceFqdn   string   `json:"active_pce_fqdn,omitempty"`
-// 	AgentVersion    string   `json:"agent_version,omitempty"`
-// 	Description     string   `json:"description,omitempty"`
-// 	Hostname        string   `json:"hostname,omitempty"`
-// 	IPTablesSaved   bool     `json:"ip_tables_saved,omitempty"`
-// 	Labels          []*Label `json:"labels,omitempty"`
-// 	LastHeartbeatOn string   `json:"last_heartbeat_on,omitempty"`
-// 	LogTraffic      bool     `json:"log_traffic,omitempty"`
-// 	Mode            string   `json:"mode,omitempty"`
-// 	Name            string   `json:"name,omitempty"`
-// 	Online          bool     `json:"online,omitempty"`
-// 	OsDetail        string   `json:"os_detail,omitempty"`
-// 	OSID            string   `json:"os_id,omitempty"`
-// 	PublicIP        string   `json:"public_ip,omitempty"`
-// 	TargetPceFqdn   string   `json:"target_pce_fqdn,omitempty"`
-// 	UID             string   `json:"uid,omitempty"`
-// 	UptimeSeconds   string   `json:"uptime_seconds,omitempty"`
-// }
-
 // CompatibilityReport is a compatibility report for a VEN in Idle status
 type CompatibilityReport struct {
 	LastUpdatedAt time.Time `json:"last_updated_at"`
