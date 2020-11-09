a13354cac2964dff7b04a8ba94a3c0e4928e82e1
Brian Pitta <Brian.Pitta@illumio.com>
2020-11-09 10:00:24 -0500

    enhanced functionality for user groups

--- usergroup.go
+++ usergroup.go
@@ -7,41 +7,68 @@ import (
 	"strconv"
 )
 
-// ADUserGroup represents an AD group used as policy oject in the PCE
-type ADUserGroup struct {
-	Href          string `json:"href,omitempty"`
-	Deleted       bool   `json:"deleted,omitempty"`
-	UsedByRuleSet bool   `json:"used_by_ruleset,omitempty"`
-	Name          string `json:"name,omitempty"`
-	Sid           string `json:"sid,omitempty"`
-	Description   string `json:"description,omitempty"`
+// GetAllADUserGroups gets all user groups in the PCE
+func (p *PCE) GetAllADUserGroups() ([]ConsumingSecurityPrincipals, APIResponse, error) {
+
+	// Build the API URL
+	apiURL, err := url.Parse("https://" + pceSanitization(p.FQDN) + ":" + strconv.Itoa(p.Port) + "/api/v2/orgs/" + strconv.Itoa(p.Org) + "/security_principals")
+	if err != nil {
+		return nil, APIResponse{}, fmt.Errorf("GetAllADUserGroups - %s", err)
+	}
+
+	// Call the API
+	api, err := apicall("GET", apiURL.String(), *p, nil, false)
+	if err != nil {
+		return nil, api, fmt.Errorf("GetAllADUserGroups - %s", err)
+	}
+
+	// Unmarshal response to struct
+	var adUserGroups []ConsumingSecurityPrincipals
+	json.Unmarshal([]byte(api.RespBody), &adUserGroups)
+
+	// If adUserGroups is 500, re-run with async
+	if len(adUserGroups) >= 500 {
+		api, err = apicall("GET", apiURL.String(), *p, nil, true)
+		if err != nil {
+			return nil, api, fmt.Errorf("adUserGroups - %s", err)
+		}
+
+		// Unmarshal response to struct
+		var asyncADUserGroups []ConsumingSecurityPrincipals
+		json.Unmarshal([]byte(api.RespBody), &asyncADUserGroups)
+
+		return asyncADUserGroups, api, nil
+	}
+
+	// Return if less than 500
+	return adUserGroups, api, nil
 }
 
 // CreateADUserGroup creates a user group policy object in the PCE
-func (p *PCE) CreateADUserGroup(g ADUserGroup) (ADUserGroup, APIResponse, error) {
+func (p *PCE) CreateADUserGroup(g ConsumingSecurityPrincipals) (ConsumingSecurityPrincipals, APIResponse, error) {
 	var api APIResponse
 	var err error
 
 	// Build the API URL
 	apiURL, err := url.Parse("https://" + pceSanitization(p.FQDN) + ":" + strconv.Itoa(p.Port) + "/api/v2/orgs/" + strconv.Itoa(p.Org) + "/security_principals")
 	if err != nil {
-		return ADUserGroup{}, api, fmt.Errorf("CreateADUserGroup - %s", err)
+		return ConsumingSecurityPrincipals{}, api, fmt.Errorf("CreateADUserGroup - %s", err)
 	}
 
 	// Create payload
 	userGroupJSON, err := json.Marshal(g)
 	if err != nil {
-		return ADUserGroup{}, api, fmt.Errorf("CreateADUserGroup - %s", err)
+		return ConsumingSecurityPrincipals{}, api, fmt.Errorf("CreateADUserGroup - %s", err)
 	}
 
 	// Call the API
 	api, err = apicall("POST", apiURL.String(), *p, userGroupJSON, false)
 	if err != nil {
-		return ADUserGroup{}, api, fmt.Errorf("CreateADUserGroup - %s", err)
+		return ConsumingSecurityPrincipals{}, api, fmt.Errorf("CreateADUserGroup - %s", err)
 	}
 
 	// Unmarshal new label
-	var newGroup ADUserGroup
+	var newGroup ConsumingSecurityPrincipals
 	json.Unmarshal([]byte(api.RespBody), &newGroup)
 
 	return newGroup, api, nil
