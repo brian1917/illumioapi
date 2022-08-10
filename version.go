2f2e5eba3123e2b554d09e1bb6ea59d8d3a50b5c
Brian Pitta <brianpitta@gmail.com>
2022-08-10 19:19:12 -0400

    added api response

--- version.go
+++ version.go
@@ -20,18 +20,18 @@ type Version struct {
 }
 
 // GetVersion returns the version of the PCE
-func (p *PCE) GetVersion() (version Version, err error) {
+func (p *PCE) GetVersion() (version Version, api APIResponse, err error) {
 
 	// Build the API URL
 	apiURL, err := url.Parse("https://" + p.cleanFQDN() + ":" + strconv.Itoa(p.Port) + "/api/v2/product_version")
 	if err != nil {
-		return Version{}, fmt.Errorf("get version - %s", err)
+		return Version{}, api, fmt.Errorf("get version - %s", err)
 	}
 
 	// Call the API
-	api, err := p.httpReq("GET", apiURL.String(), nil, false, true)
+	api, err = p.httpReq("GET", apiURL.String(), nil, false, true)
 	if err != nil {
-		return Version{}, fmt.Errorf("get version - %s", err)
+		return Version{}, api, fmt.Errorf("get version - %s", err)
 	}
 	json.Unmarshal([]byte(api.RespBody), &version)
 
@@ -39,20 +39,20 @@ func (p *PCE) GetVersion() (version Version, err error) {
 	numbers := strings.Split(version.Version, ".")
 	version.Major, err = strconv.Atoi(numbers[0])
 	if err != nil {
-		return Version{}, fmt.Errorf("calculating major - %s", err)
+		return Version{}, api, fmt.Errorf("calculating major - %s", err)
 	}
 	if len(numbers) > 1 {
 		version.Minor, err = strconv.Atoi(numbers[1])
 		if err != nil {
-			return Version{}, fmt.Errorf("calculating minor - %s", err)
+			return Version{}, api, fmt.Errorf("calculating minor - %s", err)
 		}
 	}
 	if len(numbers) > 2 {
 		version.Patch, err = strconv.Atoi(numbers[2])
 		if err != nil {
-			return Version{}, fmt.Errorf("calculating patch - %s", err)
+			return Version{}, api, fmt.Errorf("calculating patch - %s", err)
 		}
 	}
 
-	return version, nil
+	return version, api, nil
 }
