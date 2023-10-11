3115e50c3a70361f661c27e3c090df3d904b3c2a
Brian Pitta <brianpitta@gmail.com>
2023-10-11 10:00:38 -0400

    updates

--- usergroup.go
+++ usergroup.go
@@ -3,6 +3,7 @@ package illumioapi
 // ConsumingSecurityPrincipals are AD user groups
 type ConsumingSecurityPrincipals struct {
 	Deleted       bool   `json:"deleted,omitempty"`
+	Description   string `json:"description,omitempty"`
 	Href          string `json:"href,omitempty"`
 	Name          string `json:"name,omitempty"`
 	SID           string `json:"sid,omitempty"`
@@ -23,6 +24,7 @@ func (p *PCE) GetADUserGroups(queryParameters map[string]string) (api APIRespons
 	for _, cp := range p.ConsumingSecurityPrincipalsSlice {
 		p.ConsumingSecurityPrincipals[cp.Href] = cp
 		p.ConsumingSecurityPrincipals[cp.Name] = cp
+		p.ConsumingSecurityPrincipals[cp.SID] = cp
 	}
 	return api, err
 }
@@ -31,4 +33,18 @@ func (p *PCE) GetADUserGroups(queryParameters map[string]string) (api APIRespons
 func (p *PCE) CreateADUserGroup(group ConsumingSecurityPrincipals) (createdGroup ConsumingSecurityPrincipals, api APIResponse, err error) {
 	api, err = p.Post("security_principals", &group, &createdGroup)
 	return createdGroup, api, err
+}
+
+// UpdateADUserGroup updates an existing AD user group in the PCE.
+// The provided ad user group must include an Href.
+// Properties that cannot be included in the PUT method will be ignored.
+func (p *PCE) UpdateADUserGroup(group ConsumingSecurityPrincipals) (APIResponse, error) {
+	// Create a new ad user group with just the fields that should be updated and the href
+	g := ConsumingSecurityPrincipals{
+		Href:        group.Href,
+		Description: group.Description,
+		Name:        group.Name,
+	}
+	api, err := p.Put(&g)
+	return api, err
 }
